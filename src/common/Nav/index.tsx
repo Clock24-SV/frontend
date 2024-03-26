9e6997ce63b037e8840a0c938f3ae30de99f2166
seunghwan <sun1238799@naver.com>
2024-03-26 23:59:08 +0900

    design: side bar 디자인

--- src/common/Nav/index.tsx
+++ src/common/Nav/index.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import classNames from "classnames/bind";
 import logo from "../../assets/Images/nav-logo.svg";
 import config from "../../assets/Images/config.svg";
 import styles from "./index.module.scss";
 import RImage from "../RImage";
+import { IoClose } from "react-icons/io5";
+import { GiHamburgerMenu } from "react-icons/gi";
 import Image from "next/image";
+import { AnimatePresence, motion } from "framer-motion";
 
 const cx = classNames.bind(styles);
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className={cx("nav")}>
       <div className={cx("inner-nav")}>
@@ -26,6 +33,28 @@ const Nav = () => {
           </div>
           <div className={cx("profile")}></div>
         </div>
+        <GiHamburgerMenu
+          onClick={() => setIsOpen((pre) => !pre)}
+          className={cx("mobile-toggle-icon")}
+        />
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div className={cx("sidebar-back")}>
+              <motion.div
+                className={cx("sidebar")}
+                transition={{ type: "just", stiffness: 100 }}
+                initial={{ x: "100%" }}
+                animate={{ x: "0" }}
+                exit={{ x: "100%" }}
+              >
+                <IoClose
+                  onClick={() => setIsOpen((pre) => !pre)}
+                  className={cx("close-sidebar")}
+                />
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </nav>
   );
