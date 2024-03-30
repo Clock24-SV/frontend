149c13687b593499ed8ca6f2a02eb64914515d75
EunJu JO <dmswnehf@naver.com>
2024-03-30 22:25:45 +0900

    style: className 수정

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -9,13 +9,13 @@ const cx = classnames.bind(style);
 export default function Home() {
   return (
     <main className={cx("page")}>
-      <div className={cx("imgWrapper")}>
+      <div className={cx("img-wrapper")}>
         <Image src={logo} alt="logo" priority={true} fill={true} />
       </div>
 
       <h1 className={cx("title")}>Clock24</h1>
 
-      <div className={cx("subWrapper")}>
+      <div className={cx("sub-wrapper")}>
         <span className={cx("sub")}>하루 24시간,</span>
         <span className={cx("sub")}>달성 실패 시 메시지가 전송돼요.</span>
       </div>
