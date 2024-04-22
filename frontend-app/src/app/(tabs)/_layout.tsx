511e83701f8f94b520a93f4d239de9ccc10f23b2
seunghwan <sun1238799@naver.com>
2024-04-22 16:06:26 +0900

    design: header / nav 퍼블리싱

--- frontend-app/src/app/(tabs)/_layout.tsx
+++ frontend-app/src/app/(tabs)/_layout.tsx
@@ -3,30 +3,81 @@ import { Tabs } from "expo-router";
 import { colors } from "@/src/constants/Colors";
 import { useColorScheme } from "@/src/components/useColorScheme";
 import { useClientOnlyValue } from "@/src/components/useClientOnlyValue";
+import styled, { css } from "@emotion/native";
+import CustomText from "@/src/common/CustomText";
+import { TABLISTS } from "@/src/constants/tab";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: colors[colorScheme ?? "light"].tint,
+        tabBarActiveTintColor: "black",
+        tabBarInactiveTintColor: "gray",
         headerShown: useClientOnlyValue(false, true),
+        tabBarStyle: {
+          backgroundColor: colors[colorScheme ?? "light"].background,
+          paddingTop: 3,
+          paddingBottom: 3,
+          alignItems: "center",
+          justifyContent: "center",
+        },
       }}
     >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Tab One",
-          // tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: "Tab Two",
-          // tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
+      {TABLISTS.map(({ name, title, icon }) => (
+        <Tabs.Screen
+          key={name}
+          name={name}
+          options={{
+            title,
+            header: () => (
+              <Header>
+                <InlineHeader>
+                  <CustomText style={HeaderText}>{title}</CustomText>
+                  <ProfileImage
+                    source={require("../../assets/images/profile.png")}
+                    resizeMode="contain"
+                  />
+                </InlineHeader>
+              </Header>
+            ),
+            tabBarIcon: ({ focused }) => (
+              <StyledImage focused={focused} source={icon} />
+            ),
+          }}
+        />
+      ))}
     </Tabs>
   );
 }
+
+const StyledImage = styled.Image<{ focused: boolean }>(({ focused }) => ({
+  width: 25,
+  height: 25,
+  padding: 5,
+  resizeMode: "contain",
+  opacity: focused ? 1 : 0.5,
+}));
+
+const ProfileImage = styled.Image`
+  width: 30px;
+  height: 30px;
+`;
+
+const Header = styled.SafeAreaView`
+  background-color: ${colors.WHITE};
+`;
+
+const InlineHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%;
+  margin: 0 auto;
+  height: 50px;
+`;
+
+const HeaderText = css`
+  font-size: 20px;
+`;
---
+++ frontend-app/src/constants/tab.ts
@@ -0,0 +1,18 @@
+export const TABLISTS = [
+  { name: "index", title: "홈", icon: require("../assets/images/Home.png") },
+  {
+    name: "FriendSearch",
+    title: "친구 찾기",
+    icon: require("../assets/images/Search.png"),
+  },
+  {
+    name: "FriendList",
+    title: "친구 목록",
+    icon: require("../assets/images/FriendAdd.png"),
+  },
+  {
+    name: "Config",
+    title: "환경설정",
+    icon: require("../assets/images/Config.png"),
+  },
+] as const;
